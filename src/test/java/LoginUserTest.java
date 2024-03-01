975e8248274dbc3059dbe665c23da27ce5c572b6
Amirdjan94 <Amirdjan94@gmail.com>
2024-03-01 23:49:19 +0500

    Add allure report and add annotations for allure

--- src/test/java/LoginUserTest.java
+++ src/test/java/LoginUserTest.java
@@ -1,17 +1,20 @@
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pom.ItemsListScreen;
 import pom.MainScreen;
 
-
 public class LoginUserTest extends BaseTest {
     @Test
+    @DisplayName("#1 Сheck login user")
+    @Description("Basic test for login user in app")
+
     public void logInUserForFillInput() {
 
         MainScreen mainScreen = new MainScreen();
         driver.findElement(mainScreen.standard_userBtn).click();
         driver.findElement(mainScreen.loginBtn).click();
-
         Assertions.assertTrue(new ItemsListScreen(driver).isVisiblebasketBtn());
 
     }
